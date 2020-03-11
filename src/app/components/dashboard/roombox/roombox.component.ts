12017c6be57d9b94f1ba6cc476b079d9a83e2cae
Joerg Hannemann <joerg@Joergs-MBP.fritz.box>
2020-03-11 13:42:15 +0100

    updated small changes in design

--- src/app/components/dashboard/roombox/roombox.component.ts
+++ src/app/components/dashboard/roombox/roombox.component.ts
@@ -6,7 +6,7 @@ import { AppComponent } from 'src/app/app.component';
 @Component({
   selector: 'app-roombox',
   templateUrl: './roombox.component.html',
-  styleUrls: ['./roombox.component.css']
+  styleUrls: ['./roombox2.component.css']
 })
 export class RoomboxComponent implements OnInit {
   @Input() shape: string;
@@ -22,35 +22,38 @@ export class RoomboxComponent implements OnInit {
   }
 
   ngOnChanges() {
-    // check battery
-    this.data.filter(i => i.category == "battery").forEach(item => {
-      var threshold = this.dataTypes.filter(t => t.category == "battery" && item.groupNames.includes(t.name))[0].warningThreshold;
-      if (!threshold) {
-        threshold = 0;
+    if (this.data) {
+      // check battery
+      this.data.filter(i => i.category == "battery").forEach(item => {
+        var threshold = this.dataTypes.filter(t => t.category == "battery" && item.groupNames.includes(t.name))[0].warningThreshold;
+        if (!threshold) {
+          threshold = 0;
+        }
+        if (Number.parseInt(item.state) < threshold) {
+          this.batteryWarning = true;
+        }
+      });
+      // refine item transformedState 
+      this.data.forEach(item =>{
+        if (!item.transformedState) {
+          item.transformedState = item.state;
+        }
+        if (item.transformedState.length > 8) {
+          item.transformedState = item.transformedState.substr(0, 6)+"...";
+        }
+        if (item.unit) {
+          item.transformedState = `${item.transformedState} ${item.unit}`;
+        }
+      });
+      // only unique Categories for UI
+      this.categories = [...new Set(this.data.map(d => d.category))].sort();
+      // replace item labels for UI (maybe wrong place here!?)
+      this.dataTypes.forEach(dataType => {
+        if (dataType.replaceInItemLabel) {
+          dataType.replaceInItemLabel.forEach(label => this.data.forEach(item => item.label = item.label.replace(label, '')));
+        }
+      });
       }
-      if (Number.parseInt(item.state) < threshold) {
-        this.batteryWarning = true;
-      }
-    });
-    // refine item transformedState 
-    this.data.forEach(item =>{
-      if (!item.transformedState) {
-        item.transformedState = item.state;
-      }
-      if (item.transformedState.length > 8) {
-        item.transformedState = item.transformedState.substr(0, 6)+"...";
-      }
-      if (item.unit) {
-        item.transformedState = `${item.transformedState} ${item.unit}`;
-      }
-    });
-    // only unique Categories for UI
-    this.categories = [...new Set(this.data.map(d => d.category))].sort();
-    // replace item labels for UI (maybe wrong place here!?)
-    this.dataTypes.forEach(dataType => {
-      if (dataType.replaceInItemLabel) {
-        dataType.replaceInItemLabel.forEach(label => this.data.forEach(item => item.label = item.label.replace(label, '')));
-      }
-    });
-  }
+    }
+    
 }
