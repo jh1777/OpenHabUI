961a8b009cf8c08a210c6bf9b210f34e7a9b9047
Joerg Hannemann <joerg@Joergs-MBP.fritz.box>
2020-03-27 22:15:39 +0100

    added isGroup flag to config

--- src/app/models/config/item.ts
+++ src/app/models/config/item.ts
@@ -1,10 +1,20 @@
-export class Item {  // V3
+export class Item { 
+    // Name of the OpenHab Item or Group
     name: string;
+    // Visual representation in UI
     displayName: string;
+    // UI Category - see Readme
     category: string;
+    // Optional: Unit to display (e.g. "%", "°C", etc.)
     unit?: string;
+    // Optional: Is this item a group in OpenHab?
+    isGroup?: boolean;
+    // Warning threshold value until it gets in Status hasWarning
     warningThreshold?: number;
+    // Threshold typ ("lt", "gt" supported)
     warningThresholdType?: string;
+    // Show this Item in Summary Bar (don't use if you use showOnlyInSummary)
     showInSummary?: boolean = false;
+    // Show this Item in Summary Bar ONLY, not in any Tiles (don't use if you use showInSummary)
     showOnlyInSummary?: boolean = false;
 }
