975d9faf20bc6d804b20b7215dcd3aae5d22ef28
Joerg Hannemann <joerg@Joergs-MBP.fritz.box>
2020-07-27 22:01:13 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { RoomboxComponent } from './components/dashboard/roombox/roombox.component';
 import { TileComponent } from './components/dashboard/tile/tile.component';
 import { ItemiconComponent } from './components/itemicon/itemicon.component';
 import { SummaryComponent } from './components/dashboard/summary/summary.component';
@@ -21,7 +20,6 @@ import { ItemDetailsComponent } from './components/dashboard/item-details/item-d
   declarations: [
     AppComponent,
     DashboardComponent,
-    RoomboxComponent,
     TileComponent,
     ItemiconComponent,
     SummaryComponent,
